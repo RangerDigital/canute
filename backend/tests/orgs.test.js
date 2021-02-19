d25d71dab4e2ae190cb9d213d7ec30037242310d
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-02-19 18:07:05 +0100

    Add /orgs tests

---
+++ backend/tests/orgs.test.js
@@ -0,0 +1,40 @@
+const db = require('./db');
+const app = require('../server');
+
+const { getAuthToken } = require('./helpers');
+
+const supertest = require('supertest');
+const request = supertest(app);
+
+beforeAll(async () => await db.connect());
+beforeEach(async () => await db.clear());
+afterAll(async () => await db.close());
+
+describe('Orgs Resources', function () {
+  it('Positive: Check GET: /orgs without belonging to any', async () => {
+    {
+      const authToken = getAuthToken();
+
+      const res = await request.get('/orgs').set('Authorization', 'Bearer ' + authToken);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toStrictEqual([]);
+    }
+  });
+
+  it('Positive: Check POST: /orgs. Create a new organisation', async () => {
+    {
+      const authToken = getAuthToken();
+      const name = 'Vanguard';
+
+      const res = await request
+        .post('/orgs')
+        .set('Authorization', 'Bearer ' + authToken)
+        .send({ name: name });
+
+      expect(res.status).toBe(200);
+      expect(res.body.name).toBe(name);
+      expect(res.body.users[0].isAdmin).toBe(true);
+    }
+  });
+});
