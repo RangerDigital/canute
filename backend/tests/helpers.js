23ccbf496e0c4c05e27d22d98aa16f35ca49cc37
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-02-19 17:18:20 +0100

    Trigger workflow

--- backend/tests/helpers.js
+++ backend/tests/helpers.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const users = require('../models/users');
 
 const getAuthToken = () => {
-  let user = new users({ email: 'testing@gmail.com', name: 'Test User' });
+  let user = new users({ email: 'testing@gmail.com', name: 'User' });
   user.save();
 
   return jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1w' });
