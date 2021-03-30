53bb71f509645a9f109ace834585b5f386c9eb70
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-03-30 15:21:29 +0200

    Add w-60 to AppNavigation for longer orgs

--- frontend/src/components/AppNavigation.vue
+++ frontend/src/components/AppNavigation.vue
@@ -1,14 +1,12 @@
 <template
   ><!-- Left Bar-->
-  <nav class="flex flex-col">
+  <nav class="flex flex-col w-60">
     <!-- Organisation -->
     <div class="my-2 xl:my-5 2xl:my-12 flex flex-row">
       <div class="bg-gray-dark w-px mr-5 rounded-full"></div>
 
       <div class="flex flex-col justify-around">
-        <h2 class="py-2 font-sans font-semibold text-white text-xl"
-          >Hello! <span class="text-red">{{ name }}</span></h2
-        >
+        <h2 class="py-2 font-sans font-semibold text-red text-xl">{{ name }}</h2>
         <p class="my-2 text-sm font-medium text-gray">{{ address }}</p>
       </div>
     </div>
@@ -19,13 +17,13 @@
 
       <div class="flex flex-col justify-around">
         <div class="my-5 2xl:my-12 flex flex-col justify-around">
-          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/auth">Things</router-link>
-          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/auth">Dashboard</router-link>
+          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/things">Things</router-link>
+          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/dashboard">Dashboard</router-link>
         </div>
         <div class="my-5 2xl:my-12 flex flex-col justify-around">
-          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/auth">Profile</router-link>
-          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/auth">Settings</router-link>
-          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/auth">Organisations</router-link>
+          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/profile">Profile</router-link>
+          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/settings">Settings</router-link>
+          <router-link class="my-2 text-sm font-medium text-gray hover:text-gray-light" to="/organisations">Organisations</router-link>
         </div>
       </div>
     </div>
@@ -34,27 +32,19 @@
 <script>
   export default {
     name: 'AppNavigation',
-    props: {
-      organisation: String,
-    },
     data() {
       return {
         name: '',
         address: 'Rosłońskiego, Przemyśl',
+        organisation: '',
       };
     },
-    watch: {
-      organisation: {
-        immediate: true,
-        // TO DO: Add address getter
-        handler(organisation) {
-          console.log('Getting name from', organisation);
+    mounted() {
+      this.organisation = localStorage.organisation;
 
-          this.axios.get('/api/orgs/' + organisation).then((payload) => {
-            this.name = payload.data.name;
-          });
-        },
-      },
+      this.axios.get('/api/orgs/' + this.organisation).then((payload) => {
+        this.name = payload.data.name;
+      });
     },
   };
 </script>
