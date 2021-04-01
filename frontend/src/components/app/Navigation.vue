447d39dc3445046fff39b845d392644001fb79a2
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-01 20:13:10 +0200

    Make navigation button full width

--- frontend/src/components/app/Navigation.vue
+++ frontend/src/components/app/Navigation.vue
@@ -3,13 +3,13 @@
   <div class="flex flex-row">
     <div class="bg-gray-dark w-px mr-5 rounded-full"></div>
 
-    <div class="flex flex-col justify-around">
+    <div class="w-full flex flex-col justify-around">
       <div class="my-5 2xl:my-12 flex flex-col justify-around">
         <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/dashboard">Dashboard</router-link>
       </div>
 
       <!-- Admin Navigation -->
-      <div v-if="organisationAdmin" class="flex flex-col justify-around">
+      <div v-if="organisationAdmin" class="w-full flex flex-col justify-around">
         <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/roles">Roles</router-link>
         <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/users">Users</router-link>
         <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/devices">Devices</router-link>
