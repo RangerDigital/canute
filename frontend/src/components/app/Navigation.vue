218c0a46b4a0327cdada72df3a1e8811b9d48702
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-04 16:18:54 +0200

    Change margin to padding in Navigation.vue

--- frontend/src/components/app/Navigation.vue
+++ frontend/src/components/app/Navigation.vue
@@ -5,19 +5,19 @@
 
     <div class="w-full flex flex-col justify-around">
       <div class="my-5 2xl:my-12 flex flex-col justify-around">
-        <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/dashboard">Dashboard</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/dashboard">Dashboard</router-link>
       </div>
 
       <!-- Admin Navigation -->
       <div v-if="organisationAdmin" class="w-full flex flex-col justify-around">
-        <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/roles">Roles</router-link>
-        <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/users">Users</router-link>
-        <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/devices">Devices</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/roles">Roles</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/users">Users</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/devices">Devices</router-link>
       </div>
 
       <div class="my-6 2xl:my-12 flex flex-col justify-around">
-        <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/guard">Settings</router-link>
-        <router-link class="my-4 xl:my-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/organisations">Organisations</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/guard">Settings</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/organisations">Organisations</router-link>
       </div>
     </div>
   </div>
