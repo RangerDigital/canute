9f0ba2de9e933a837ef1b02142ed0eba54587c37
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-04 21:08:47 +0200

    Navigation locale

--- frontend/src/components/app/Navigation.vue
+++ frontend/src/components/app/Navigation.vue
@@ -5,19 +5,21 @@
 
     <div class="w-full flex flex-col justify-around">
       <div class="my-5 2xl:my-12 flex flex-col justify-around">
-        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/dashboard">Dashboard</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/dashboard">{{ $t('nav.dashboard') }}</router-link>
       </div>
 
       <!-- Admin Navigation -->
       <div v-if="organisationAdmin" class="w-full flex flex-col justify-around">
-        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/groups">Groups</router-link>
-        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/users">Users</router-link>
-        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/devices">Devices</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/groups">{{ $t('nav.groups') }}</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/users">{{ $t('nav.users') }}</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/devices">{{ $t('nav.devices') }}</router-link>
       </div>
 
       <div class="my-6 2xl:my-12 flex flex-col justify-around">
-        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/guard">Settings</router-link>
-        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/organisations">Organisations</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/guard">{{ $t('nav.settings') }}</router-link>
+        <router-link class="py-4 xl:py-2 text-sm font-medium text-gray hover:text-gray-light" active-class="text-gray-light" to="/organisations">{{
+          $t('nav.organisations')
+        }}</router-link>
       </div>
     </div>
   </div>
