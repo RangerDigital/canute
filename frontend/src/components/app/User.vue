82d0ba2e849d3ae13b146e9f605175f30db2d1c2
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-04 18:16:38 +0200

    Add truncate to User.vue

--- frontend/src/components/app/User.vue
+++ frontend/src/components/app/User.vue
@@ -2,7 +2,7 @@
   <div class="w-full px-4 py-2 my-3 xl:px-6 xl:py-4 border rounded-lg border-gray-dark flex flex-col justify-between cursor-pointer hover:border-red" @click="editUser()">
     <h2 class="py-2  text-white ">{{ user.user.email }}</h2>
     <p class="my-1 text-sm  text-red">{{ user.annotation }}</p>
-    <p class="my-1 text-sm  text-gray-dark">
+    <p class="my-1 text-sm truncate text-gray-dark">
       <span class="mr-4" v-if="user.isAdmin">Admin</span>
       <span class="mr-4" v-for="item in user.roles" :key="item._id">{{ item.name }}</span>
     </p>
