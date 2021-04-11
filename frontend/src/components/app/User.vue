96fcfc0a2fa231398826b87fc0967a94556d20a1
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-11 17:18:41 +0200

    Fix missing email in User

--- frontend/src/components/app/User.vue
+++ frontend/src/components/app/User.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full px-4 py-2 my-3 xl:px-6 xl:py-4 border rounded-lg border-gray-darker flex flex-col justify-between cursor-pointer hover:border-red">
-    <h2 class="py-2  text-white ">{{ user.user.email }}</h2>
+    <h2 class="py-2  text-white ">{{ user.email }}</h2>
     <p class="my-1 text-sm  text-red">{{ user.annotation }}</p>
     <p class="my-1 text-sm truncate text-gray-dark">
       <span class="mr-4" v-if="user.isAdmin">Admin</span>
