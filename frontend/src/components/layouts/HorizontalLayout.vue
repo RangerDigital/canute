92f7364006b4a5fed0adb4fc75a355b4a56c0837
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-11 17:00:32 +0200

    Add HorizontalLayout

---
+++ frontend/src/components/layouts/HorizontalLayout.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="min-h-screen h-full flex flex-col justify-between">
+    <HomeNavigation noLogin noNav />
+
+    <div class="self-center xl:self-auto w-full h-full flex-grow md:max-w-md xl:max-w-full flex flex-col xl:flex-row justify-start xl:justify-between xl:px-6 xl:py-6">
+      <AppNavigation class="hidden xl:flex" />
+
+      <slot></slot>
+    </div>
+
+    <HomeFooter />
+  </section>
+</template>
+
+<script>
+  import HomeNavigation from '@/components/HomeNavigation.vue';
+  import HomeFooter from '@/components/HomeFooter.vue';
+  import AppNavigation from '@/components/AppNavigation.vue';
+
+  export default {
+    name: 'HorizontalLayout',
+    components: {
+      HomeNavigation,
+      HomeFooter,
+      AppNavigation,
+    },
+  };
+</script>
--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -1,62 +1,51 @@
 <template>
-  <section class="min-h-screen h-full flex flex-col justify-between">
-    <HomeNavigation noLogin noNav />
+  <HorizontalLayout>
+    <!-- Vertical Container -->
+    <div class="w-full p-4 xl:p-12  xl:mx-0 2xl:mx-20 flex flex-col justify-start">
+      <div>
+        <h1 class="xl:mx-5 py-2 font-sans text-gray-dark text-sm"
+          ><svg class="inline mr-2 h-5 w-5 text-gray-dark" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.2"
+              d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+            /></svg
+          >Manage Users</h1
+        >
 
-    <!-- Horizontal Main Container -->
-    <div class="self-center xl:self-auto w-full h-full flex-grow md:max-w-md xl:max-w-full flex flex-col xl:flex-row justify-start xl:justify-between xl:px-6 xl:py-6">
-      <AppNavigation class="hidden xl:flex" />
+        <div class="w-full flex flex-row justify-between">
+          <input
+            v-model="search"
+            placeholder="Search"
+            class="w-full xl:max-w-md xl:mx-5 block border-transparent focus:outline-none text-sm bg-gray-darker font-base tracking-wide px-3 py-2.5 text-white rounded-md placeholder-gray-dark ring-red focus:ring-1 "
+          />
 
-      <!-- Vertical Container -->
-      <div class="w-full p-4 xl:p-12  xl:mx-0 2xl:mx-20 flex flex-col justify-start">
-        <div>
-          <h1 class="xl:mx-5 py-2 font-sans text-gray-dark text-sm"
-            ><svg class="inline mr-2 h-5 w-5 text-gray-dark" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <button
+            class="font-medium text-sm ml-2 lg:mx-5  py-3 lg:my-0 px-3 xl:px-9 text-white bg-red hover:bg-red-dark rounded-md focus:outline-none"
+            @click="$router.push('/users/create')"
+            ><svg class="inline xl:transform xl:scale-125 h-6 xl:h-4 xl:mr-4 align-middle text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                stroke-width="1.2"
-                d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+                stroke-width="1.5"
+                d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
               /></svg
-            >Manage Users</h1
+            ><span class="hidden xl:inline">ADD USER</span></button
           >
-
-          <div class="w-full flex flex-row justify-between">
-            <input
-              v-model="search"
-              placeholder="Search"
-              class="w-full xl:max-w-md xl:mx-5 block border-transparent focus:outline-none text-sm bg-gray-darker font-base tracking-wide px-3 py-2.5 text-white rounded-md placeholder-gray-dark ring-red focus:ring-1 "
-            />
-
-            <button
-              class="font-medium text-sm ml-2 lg:mx-5  py-3 lg:my-0 px-3 xl:px-9 text-white bg-red hover:bg-red-dark rounded-md focus:outline-none"
-              @click="$router.push('/users/create')"
-              ><svg class="inline xl:transform xl:scale-125 h-6 xl:h-4 xl:mr-4 align-middle text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="1.5"
-                  d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
-                /></svg
-              ><span class="hidden xl:inline">ADD USER</span></button
-            >
-          </div>
-          <div class="xl:block bg-gray-darker my-5 h-px w-full rounded-full"></div>
         </div>
+        <div class="xl:block bg-gray-darker my-5 h-px w-full rounded-full"></div>
+      </div>
 
-        <div class="justify-items-center grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 lg:gap-6">
-          <User class="max-w-sm xl:mx-4" v-for="item in searchedUsers" :key="item._id" v-bind:user="item" />
-        </div>
+      <div class="justify-items-center grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 3xl:grid-cols-4 lg:gap-6">
+        <User class="max-w-sm xl:mx-4" v-for="item in searchedUsers" :key="item._id" v-bind:user="item" />
       </div>
     </div>
-
-    <HomeFooter />
-  </section>
+  </HorizontalLayout>
 </template>
 
 <script>
-  import HomeNavigation from '@/components/HomeNavigation.vue';
-  import AppNavigation from '@/components/AppNavigation.vue';
-  import HomeFooter from '@/components/HomeFooter.vue';
+  import HorizontalLayout from '@/components/layouts/HorizontalLayout.vue';
 
   import User from '@/components/app/User.vue';
 
@@ -65,10 +54,8 @@
   export default {
     name: 'Users',
     components: {
-      HomeNavigation,
-      HomeFooter,
-      AppNavigation,
       User,
+      HorizontalLayout,
     },
     data() {
       return {
