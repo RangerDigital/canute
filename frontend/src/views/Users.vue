87bc991693ab306e1a0ba8c6848453418b50bb5c
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-14 18:28:25 +0200

    Change locale, Button icon width

--- frontend/src/views/Users.vue
+++ frontend/src/views/Users.vue
@@ -21,7 +21,7 @@
               <path
                 stroke-linecap="round"
                 stroke-linejoin="round"
-                stroke-width="1.5"
+                stroke-width="1.9"
                 d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
               /></svg
             ><span class="hidden xl:inline">{{ $t('users.add') }}</span></Button
