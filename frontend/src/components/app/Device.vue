84368eedf4e214f940d70f5cd3766e0a030e1e36
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-04-29 14:46:34 +0200

    Add device online status

--- frontend/src/components/app/Device.vue
+++ frontend/src/components/app/Device.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col justify-between w-full px-4 py-2 my-3 border rounded-lg cursor-pointer xl:px-6 xl:py-4 border-gray-darker hover:border-primary">
     <h2 class="py-2 text-white font-heading">{{ device.name }}</h2>
-    <p class="my-1 text-sm text-primary">Online </p>
+    <p class="my-1 text-sm text-primary">{{ device.online ? 'Online' : 'Offline' }}</p>
     <p class="my-1 text-xs text-gray-dark">{{ device.shadows.length }} Shadows</p>
   </div>
 </template>
