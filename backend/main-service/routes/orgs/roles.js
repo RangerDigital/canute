16a406924c44821e28aa5d896eb5aec4d9184d6c
RangerDigital <36769448+RangerDigital@users.noreply.github.com>
2021-05-09 13:48:49 +0200

    Refactor roles

--- backend/main-service/routes/orgs/roles.js
+++ backend/main-service/routes/orgs/roles.js
@@ -1,84 +1,66 @@
-const orgs = require('../../models/orgs');
+const OrgService = require('../../services/OrgService');
 
 async function routes(router) {
   router.register(require('../../hooks/adminHook'));
 
   router.get('/', async (req, res) => {
-    res.send(req.org.roles);
+    const { orgId } = req.params;
+
+    const roles = await OrgService.getRoles(orgId);
+
+    console.log(roles);
+
+    res.send(roles);
   });
 
   router.get('/:roleId', async (req, res) => {
-    const { roleId } = req.params;
+    const { orgId, roleId } = req.params;
 
-    let role = req.org.roles.filter((x) => String(x._id) == String(roleId));
+    const role = await OrgService.getOneRole(orgId, roleId);
 
-    res.send(role[0]);
+    res.send(role);
   });
 
   router.post('/', async (req, res) => {
     const { name, permissions, users } = req.body;
-    let organisation = req.org;
+    const { orgId } = req.params;
 
-    organisation.roles.push({ name: name, permissions: permissions, users: users });
+    const role = await OrgService.addRole(orgId, name, permissions, users);
 
-    await organisation.validate().catch((err) => {
-      return res.code(400).send({ msg: err });
-    });
-
-    organisation.save();
-
-    res.send(organisation.roles);
+    return res.send(role);
   });
 
   router.patch('/:roleId', async (req, res) => {
     const { name, permissions } = req.body;
     const { orgId, roleId } = req.params;
 
-    let roles = await orgs.updateOne({ _id: orgId, 'roles._id': roleId }, { $set: { 'roles.$.name': name, 'roles.$.permissions': permissions } });
+    const role = await OrgService.updateRole(orgId, roleId, name, permissions);
 
-    res.send(roles);
+    return res.send(role);
   });
 
   router.post('/:roleId/users/:userId', async (req, res) => {
-    const { roleId, userId } = req.params;
+    const { orgId, roleId, userId } = req.params;
 
-    let organisation = req.org;
+    const roles = await OrgService.addUserToRole(orgId, roleId, userId);
 
-    // Check if userId is in organisation
-
-    if (organisation.roles.find((x) => x._id == roleId).users.includes(userId)) {
-      res.send(organisation.roles);
-      return;
-    }
-    organisation.roles.find((x) => x._id == roleId).users.push(userId);
-
-    organisation.save();
-
-    res.send(organisation.roles);
+    return res.send(roles);
   });
 
   router.delete('/:roleId/users/:userId', async (req, res) => {
-    const { roleId, userId } = req.params;
+    const { orgId, roleId, userId } = req.params;
 
-    let organisation = req.org;
+    const roles = await OrgService.deleteUserFromRole(orgId, roleId, userId);
 
-    organisation.roles.find((x) => x._id == roleId).users.pull(userId);
-
-    organisation.save();
-
-    res.send(organisation);
+    return res.send(roles);
   });
 
   router.delete('/:roleId', async (req, res) => {
     const { orgId, roleId } = req.params;
 
-    let organisation = await orgs.findOne({ _id: orgId, 'roles._id': roleId });
+    const roles = await OrgService.deleteRole(orgId, roleId);
 
-    organisation.roles = organisation.roles.filter((x) => String(x._id) !== String(roleId));
-
-    organisation.save();
-
-    res.send(organisation.roles);
+    return res.send(roles);
   });
 }
 
--- backend/main-service/services/OrgService.js
+++ backend/main-service/services/OrgService.js
@@ -42,6 +42,64 @@ class OrgService {
 
     return organisation;
   }
+
+  async getRoles(orgId) {
+    const organisation = await orgs.findOne({ _id: orgId });
+
+    return organisation.roles;
+  }
+  async getOneRole(orgId, roleId) {
+    const organisation = await orgs.findOne({ _id: orgId });
+
+    const role = organisation.roles.filter((x) => String(x._id) == String(roleId));
+
+    return role[0];
+  }
+
+  async addRole(orgId, name, permissions, users) {
+    let organisation = await orgs.findOne({ _id: orgId });
+
+    organisation.roles.push({ name: name, permissions: permissions, users: users });
+
+    organisation.save();
+
+    return organisation.roles;
+  }
+
+  async updateRole(orgId, roleId, name, permissions) {
+    return await orgs.updateOne({ _id: orgId, 'roles._id': roleId }, { $set: { 'roles.$.name': name, 'roles.$.permissions': permissions } });
+  }
+
+  async deleteRole(orgId, roleId) {
+    let organisation = await orgs.findOne({ _id: orgId, 'roles._id': roleId });
+
+    organisation.roles = organisation.roles.filter((x) => String(x._id) !== String(roleId));
+    organisation.save();
+
+    return organisation.roles;
+  }
+
+  async addUserToRole(orgId, roleId, userId) {
+    let organisation = await orgs.findOne({ _id: orgId });
+
+    if (organisation.roles.find((x) => x._id == roleId).users.includes(userId)) {
+      return organisation.roles;
+    }
+
+    organisation.roles.find((x) => x._id == roleId).users.push(userId);
+    organisation.save();
+
+    return organisation.roles;
+  }
+
+  async deleteUserFromRole(orgId, roleId, userId) {
+    let organisation = await orgs.findOne({ _id: orgId });
+
+    organisation.roles.find((x) => x._id == roleId).users.pull(userId);
+    organisation.save();
+
+    return organisation.roles;
+  }
 }
 
 module.exports = new OrgService();
